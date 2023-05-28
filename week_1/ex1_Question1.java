05aec9f7f44bfac7339063e7357db7c828b13720
Sathruwan_ <102567735+SathruwanHan@users.noreply.github.com>
2023-05-28 21:28:13 +0530

    Update ex1_Question1.java
    Second commit

--- week_1/ex1_Question1.java
+++ week_1/ex1_Question1.java
@@ -1,7 +1,7 @@
 public class ex1_Question1 {
 	public static void main(String[] args) {
 		System.out.println("My name is Sathruwan");
-		System.out.println("I am in 18 years old");
+		System.out.println("I am 18 years old");
 
 	}
 
